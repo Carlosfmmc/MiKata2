febc915dc18b1e2e7599ec24e021dd7711daf981
Usuario <Usuario@10.22.148.96>
2019-10-09 13:19:04 +0100

    Histogram Calculated

--- src/mikata2/Histogram.java
+++ src/mikata2/Histogram.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mikata2;
 
-/**
- *
- * @author Usuario
- */
-class Histogram {
+import java.util.Map;
+import java.util.TreeMap;
 
-    public Histogram() {
+public class Histogram {
+    
+    Map<Integer, Integer> hist = new TreeMap();
+    
+    Histogram(int ... values) {
+        for (int value : values) {
+            hist.put(value, hist.getOrDefault(value, 0)+1);
+        }
     }
+    
     
 }
