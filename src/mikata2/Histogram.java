efc9bff0938d4f8ebee63a2e8964cfe32ddff48d
Usuario <Usuario@10.22.148.96>
2019-10-09 13:49:30 +0100

    it works and implemented the add() method

--- src/mikata2/Histogram.java
+++ src/mikata2/Histogram.java
@@ -1,9 +1,10 @@
 package mikata2;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Histogram {
+public class Histogram implements Iterable<Integer>{
     
     Map<Integer, Integer> hist = new TreeMap();
     
@@ -11,6 +12,20 @@ public class Histogram {
         for (int value : values) {
             hist.put(value, hist.getOrDefault(value, 0)+1);
         }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return hist.keySet().iterator();
+    }
+    
+
+    public int getTimes(int h1) {
+        return hist.get(h1);
+    }
+
+    public void add(int i) {
+        hist.put(i, hist.getOrDefault(i, 0)+1);
     }
     
     
