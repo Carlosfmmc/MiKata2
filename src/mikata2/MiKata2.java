692bb9efc4bab4f40472ac807087e6678ed042ed
CarlosFmmc <CarlosFmmc@DESKTOP-DEUM11Q>
2019-10-11 16:17:44 +0100

    Testing Generic histogram with Strings

--- src/mikata2/MiKata2.java
+++ src/mikata2/MiKata2.java
@@ -3,9 +3,10 @@ package mikata2;
 public class MiKata2 {//Ctrl + r para cambiarle el nombre
 
     public static void main(String[] args) {
-        Histogram histogram = new Histogram(3,1,4,5,34,2,1,-4,4,5,2,-2,4,-4,2,1,6,7,7,8,2,1);
+        Histogram histogram = new Histogram("Juan","Carlos", "Jose", "Juan", "Jose", "Carlos","Carlos", "Carlos", "Oliver", "Juan", "Carlos");
         HistogramDisplay histogramDisplay = new HistogramConsoleDisplay();
         histogramDisplay.show(histogram);
+        /**
         histogram.add(9);
         histogram.add(9);
         histogram.add(-4);
@@ -14,6 +15,7 @@ public class MiKata2 {//Ctrl + r para cambiarle el nombre
         histogram.add(9);
         histogram.remove(8);
         histogram.remove(1);
+        */
         histogramDisplay.show(histogram);//Shift + alt + flecha para mover la linea
     }
     
