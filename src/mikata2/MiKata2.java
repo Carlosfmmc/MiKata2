78aa03db272f5ebf7bd9f8974d00502f55ec529b
CarlosFmmc <CarlosFmmc@DESKTOP-DEUM11Q>
2019-10-11 17:10:23 +0100

    New NHistogram with DataObjets

--- src/mikata2/MiKata2.java
+++ src/mikata2/MiKata2.java
@@ -3,20 +3,14 @@ package mikata2;
 public class MiKata2 {//Ctrl + r para cambiarle el nombre
 
     public static void main(String[] args) {
-        Histogram histogram = new Histogram("Juan","Carlos", "Jose", "Juan", "Jose", "Carlos","Carlos", "Carlos", "Oliver", "Juan", "Carlos");
-        HistogramDisplay histogramDisplay = new HistogramConsoleDisplay();
-        histogramDisplay.show(histogram);
-        /**
-        histogram.add(9);
-        histogram.add(9);
-        histogram.add(-4);
-        histogram.add(9);
-        histogram.add(9);//Ctrl + shift + fecha para ir copiando linea
-        histogram.add(9);
-        histogram.remove(8);
-        histogram.remove(1);
-        */
-        histogramDisplay.show(histogram);//Shift + alt + flecha para mover la linea
+        NHistogram nHistogram = new NHistogram(new DataObjet('a'), new DataObjet('a'), new DataObjet('e'), new DataObjet('c'), new DataObjet('b'), new DataObjet('c'));
+        nHistogram.add(new DataObjet(9));
+        nHistogram.add(new DataObjet(9));
+        nHistogram.add(new DataObjet("y si soy el p*** amo?"));
+        NHistogramDisplay nHistogramDisplay = new NHistogramConsoleDisplay();
+        nHistogramDisplay.show(nHistogram);
+                
+                
     }
     
 }
